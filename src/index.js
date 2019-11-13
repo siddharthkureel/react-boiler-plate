4041027d87ae1fcd81924b1cc3336960faf21bd7
SiddharthKureel <joykureel@gmail.com>
2019-11-13 15:02:29 +1300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import Layout from "./components/HOC/Layout";
+import Home from "./components/Home/index";
+import "./scss/main.scss"
+const App = () =>{
+    return (
+        <Layout>
+            <Home/>
+        </Layout>
+    )
+}
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
